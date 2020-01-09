48d43358a1a65db1259f7a5dc989e8c5d08b2401
AlexVoloshenko <alex.voloshenkoua@gmail.com>
2020-01-09 18:23:18 +0200

    commit changes

---
+++ precache-manifest.2f9072b089b751d68755797bdf12f12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77469929682a4e083b8b3f9f4af9eb95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74ee125fb927062f9f66",
+    "url": "/static/css/main.1c18eb8b.chunk.css"
+  },
+  {
+    "revision": "447b55e528c42b2d0751",
+    "url": "/static/js/2.398c6cff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.398c6cff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74ee125fb927062f9f66",
+    "url": "/static/js/main.ff6f1848.chunk.js"
+  },
+  {
+    "revision": "78b544108a575579d38b",
+    "url": "/static/js/runtime-main.5bd178d9.js"
+  }
+]);
